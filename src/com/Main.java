c27d548ace3a8458449b85391cddb6bde173955e
edseds <edgars.cirulis@gmail.com>
2021-10-11 20:28:01 +0300

    Check out

--- src/com/Main.java
+++ src/com/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
     }
+
+    public static void checkout()
+    {
+        System.out.println("Check out");
+    }
 }
